011184ecc3abf594e1ef44cf2ebe0fe952ab67d3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-09 10:40:17 -0700

    docs

---
+++ doc.go
@@ -0,0 +1,4 @@
+/*
+Package webui enables an application written in the Go programming language (i.e., Golang) to create a user interface (UI) using Web technologies, such HTML, CSS, JavaScript, WebAssembly, WebRTC, etc etc etc.
+*/
+package webui
