787e2d42c3fbccf033a5055c2e358a60cd616e0e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-09 14:20:01 -0700

    docs

--- doc.go
+++ doc.go
@@ -1,4 +1,22 @@
 /*
 Package webui enables an application written in the Go programming language (i.e., Golang) to create a user interface (UI) using Web technologies, such HTML, CSS, JavaScript, WebAssembly, WebRTC, etc etc etc.
+
+Hello World Example
+
+A "hello world" example for using webui is:
+
+	import "github.com/reiver/go-webui"
+
+	// ...
+
+	err := webui.LaunchAndServeString("Hello world!")
+
+Or:
+
+	import "github.com/reiver/go-webui"
+
+	// ...
+
+	err := webui.Launch("data:,Hello%20world!")
 */
 package webui
